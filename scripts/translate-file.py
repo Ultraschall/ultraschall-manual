a35f260b11ff013622f6c06ec4e8a17d21245948
Nucleus <Nucleus-ffm@posteo.de>
2022-03-13 13:11:12 +0100

    add translate script

---
+++ scripts/translate-file.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+
+import sys
+import deepl
+import os
+import shutil
+from dotenv import load_dotenv
+load_dotenv()
+
+import sys, getopt
+
+
+# Create a Translator object providing your DeepL API authentication key.
+# To avoid writing your key in source code, you can set it in an environment
+# variable DEEPL_AUTH_KEY, then read the variable in your Python code:
+translator = deepl.Translator(os.environ['DEEPL_AUTH_KEY'])
+
+'''
+translate the given md file to en
+'''
+def translateFile(filename):
+
+   # convert file to txt
+   shutil.copy2("../docs/de/"+filename + '.md', "../docs/de/"+filename+'.txt')
+
+    # Translate a formal document from English to German
+   try:
+       translator.translate_document_from_filepath(
+           "../docs/de/"+filename+".txt",
+           "../docs/en/"+filename+".txt",
+           source_lang="DE",
+           target_lang="EN-GB"
+       )
+
+   except deepl.DocumentTranslationException as error:
+       # If an error occurs during translate_document_from_filepath() or
+       # translate_document() and after the document was already uploaded, a
+       # DocumentTranslationException is raised. The document_handle property
+       # contains the document handle to later retrieve the document or contact
+       # DeepL support.
+       doc_id = error.document_handle.id
+       doc_key = error.document_handle.key
+       print(f"Error after uploading document ${error}, id: ${doc_id} key: ${doc_key}")
+   except deepl.DeepLException as error:
+        # Errors during upload raise a DeepLException
+        print(error)
+
+
+   shutil.copy2("../docs/en/"+filename+".txt", "../docs/en/"+filename+".md")
+   # delte txt file
+   os.remove("../docs/de/"+ filename + ".txt")
+   os.remove("../docs/en/"+ filename + ".txt")
+
+
+'''
+# Glossaries allow you to customize your translations
+glossary_en_to_de = translator.create_glossary(
+    "My glossary",
+    source_lang="EN",
+    target_lang="DE",
+    entries={"artist": "Maler", "prize": "Gewinn"},
+)
+
+with_glossary = translator.translate_text_with_glossary(
+    "The artist was awarded a prize.", glossary_en_to_de
+)
+print(with_glossary)  # "Der Maler wurde mit einem Gewinn ausgezeichnet."
+
+without_glossary = translator.translate_text(
+    "The artist was awarded a prize.", target_lang="DE"
+)
+print(without_glossary)  # "Der Künstler wurde mit einem Preis ausgezeichnet."
+'''
+
+def printUsage():
+    # Check account usage
+    usage = translator.get_usage()
+    if usage.character.limit_exceeded:
+        print("Character limit exceeded.")
+    else:
+        print(f"character usage: {usage.character.count} of {usage.character.limit}")
+
+
+def main(argv):
+   inputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile="])
+   except getopt.GetoptError:
+      print ('main.py -i <inputfile>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print('main.py -i <inputfile>')
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+   print ('Input file is "', inputfile)
+
+   translateFile(inputfile)
+   printUsage()
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
