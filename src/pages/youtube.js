46340777bca723cb2fc187e48e3e01bb6ebc6c31
Nucleus <Nucleus-ffm@posteo.de>
2022-05-07 23:10:36 +0200

    fix build error with BrowserOnly tag

--- src/pages/youtube.js
+++ src/pages/youtube.js
@@ -1,34 +1,52 @@
 import React from 'react';
-import Layout from '@theme/Layout';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function youtube() {
-  const queryParams = new URLSearchParams(window.location.search)
-
-  const url = queryParams.get("url")
-  const videoURL = "https://www.youtube-nocookie.com/embed/" + url
-
-  console.log(videoURL)
     
   return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: '20px',
-          width: "560px",
-          height: "315px",
-          color: "black",
-          backgroundImage: "url(../img/video-placeholder.png)"
-        }}>
-          <body style={{borderColor: 'red', padding: "10px"}}>
-            <p>Beim Starten des Videos wird eine Verbindung mit YouTube hergestellt.
-               Die bei YouTube geltenden Datenschutzbestimmungen können
-                Sie <a href="https://policies.google.com/privacy?hl=de" target="_blank">HIER</a> einsehen.</p>
-             <form action={videoURL} target="_self">
-            <button style={{padding: "10px", border: "none", marginLeft: "37%"}} type="submit" name="start">Starte Video</button>
-            </form>
-        </body>
-      </div>  
+    <BrowserOnly fallback={<div>Loading...</div>}>
+    {()  => {
+      
+      const queryParams = new URLSearchParams(window.location.search)
+      const url = queryParams.get("url")
+      const videoURL = "https://www.youtube-nocookie.com/embed/" + url
+      console.log(videoURL)
+    
+      // check if video url is not null, else show an error message
+      if(url != null) {
+          return (
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontSize: '20px',
+                width: "560px",
+                height: "315px",
+                color: "black",
+                backgroundImage: "url(../img/video-placeholder.png)"
+              }}
+              >
+              <body style={{padding: '10px'}}>
+                <p>Beim Starten des Videos wird eine Verbindung mit YouTube hergestellt.
+                  Die bei YouTube geltenden Datenschutzbestimmungen können
+                    Sie <a href="https://policies.google.com/privacy?hl=de" target="_blank">HIER</a> einsehen.</p>
+                <form action={videoURL} target="_self">
+                <button style={{padding: '10px', border: "none", marginLeft: '37%'}} type="submit" name="start">Starte Video</button>
+                </form>
+              </body>
+           </div>  
+        )
+      } else {
+        return (
+          <div>
+            ERROR - no video URL
+          </div>
+        )
+      }
+    
+      }
+    }
+    </BrowserOnly>
   );
-}
+} 
